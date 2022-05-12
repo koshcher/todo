eaef2c63032201a2af9ad29428415cde98525b49
Koshcher <58862486+koshcher@users.noreply.github.com>
2022-05-12 10:52:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,8 +40,7 @@ for (let i = 0; i < keys.length; i++) {
                 ">
                     <i class="bi bi-check"></i>
                 </button>
-                <button class="btn btn-danger" disabled
-                onclick="localStorage.removeItem('${keys[i]}')">
+                <button class="btn btn-danger" disabled>
                     <i class="bi bi-trash"></i>
                 </button>
             </div>
@@ -61,14 +60,7 @@ for (let i = 0; i < keys.length; i++) {
                 ${ info.date != '' ? `<small class="text-muted">${info.date}</small>` : ''}
             </div>
             <div>
-                <button  class="btn btn-success" disabled
-                onclick="
-                localStorage.setItem('${keys[i]}', JSON.stringify
-                    ({
-                        date:${info.date},
-                        completed:true
-                    }));
-                ">
+                <button  class="btn btn-success" disabled>
                     <i class="bi bi-check"></i>
                 </button>
                 <button class="btn btn-danger"
@@ -80,88 +72,3 @@ for (let i = 0; i < keys.length; i++) {
         `
     }
 }
-
-
-/*
-for (let i = 0; i < keys.length; i++) {
-    let info = JSON.parse(window.localStorage.getItem(keys[i]));
-    if(info.completed == false) {
-        var taskForm = document.createElement("form");
-        taskForm.style.display = "flex";
-        taskForm.style.justifyContent = "center";
-    
-        var textP = document.createElement("p");
-        var text = document.createTextNode(keys[i]);
-        textP.appendChild(text);
-    
-        var completeBtn = document.createElement("button");
-        completeBtn.innerText = "Complete";
-        completeBtn.addEventListener("click", () => {
-            localStorage.setItem(keys[i], true);
-            this.completeBtn.disabled = true;
-            this.deleteBtn.disabled = false;
-        });
-    
-        var deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "Delete";
-        deleteBtn.addEventListener("click", () => {
-            localStorage.removeItem(keys[i]);
-        });
-    
-        if (localStorage.getItem(keys[i]) == 'true') {
-            deleteBtn.disabled = false;
-            completeBtn.disabled = true;
-        } else {
-            deleteBtn.disabled = true;
-            completeBtn.disabled = false;
-        }
-    
-        taskForm.appendChild(textP);
-        taskForm.appendChild(completeBtn);
-        taskForm.appendChild(deleteBtn);
-    
-        tasks.appendChild(taskForm);
-    }
-}
-
-for (let i = 0; i < keys.length; i++) {
-    let info = JSON.parse(window.localStorage.getItem(keys[i]));
-    if(info.completed == false) {
-        var taskForm = document.createElement("form");
-        taskForm.style.display = "flex";
-        taskForm.style.justifyContent = "center";
-    
-        var textP = document.createElement("p");
-        var text = document.createTextNode(keys[i]);
-        textP.appendChild(text);
-    
-        var completeBtn = document.createElement("button");
-        completeBtn.innerText = "Complete";
-        completeBtn.addEventListener("click", () => {
-            localStorage.setItem(keys[i], true);
-            this.completeBtn.disabled = true;
-            this.deleteBtn.disabled = false;
-        });
-    
-        var deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "Delete";
-        deleteBtn.addEventListener("click", () => {
-            localStorage.removeItem(keys[i]);
-        });
-    
-        if (localStorage.getItem(keys[i]) == 'true') {
-            deleteBtn.disabled = false;
-            completeBtn.disabled = true;
-        } else {
-            deleteBtn.disabled = true;
-            completeBtn.disabled = false;
-        }
-    
-        taskForm.appendChild(textP);
-        taskForm.appendChild(completeBtn);
-        taskForm.appendChild(deleteBtn);
-    
-        tasksDiv.appendChild(taskForm);
-    }
-}
-*/
